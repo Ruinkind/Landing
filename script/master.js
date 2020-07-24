d6bf3bf62904b3d37ffcd606fe576ed1b978f0b3
Ruinkind <terroth@gmail.com>
2020-07-24 01:08:54 -0600

    Add initial nav animation framework.  Update layout HTML framework.

---
+++ script/master.js
@@ -0,0 +1,26 @@
+window.onscroll = function() {nav()};
+
+var header = document.getElementById("nav");
+var navHeadCenter = document.getElementById("navHeadCenter");
+var navHeadSide = document.getElementsByClassName("navHeadSide");
+
+var fixed = header.offsetTop;
+
+function nav() {
+  if (window.pageYOffset > fixed) {
+    for (var i = 0; i < navHeadSide.length; i++) {
+      navHeadSide[i].classList.add("navHeadSideAni");
+      navHeadSide[i].classList.remove("navHeadSideAni2");
+    }
+    navHeadCenter.classList.add("navHeadCenterAni");
+    navHeadCenter.classList.remove("navHeadCenterAni2");
+  }
+  else {
+    for (var i = 0; i < navHeadSide.length; i++) {
+      navHeadSide[i].classList.remove("navHeadSideAni");
+      navHeadSide[i].classList.add("navHeadSideAni2");
+    }
+    navHeadCenter.classList.remove("navHeadCenterAni");
+    navHeadCenter.classList.add("navHeadCenterAni2");
+  }
+}
