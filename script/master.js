4503dbb3d634fae745f9db38642c8e38533d88a7
Ruinkind <terroth@gmail.com>
2020-08-08 00:51:24 -0600

    Add navMenu framework.

--- script/master.js
+++ script/master.js
@@ -1,12 +1,13 @@
-window.onscroll = function() {nav()};
+window.onscroll = function() {navCall()};
 
 var header = document.getElementById("navOffset");
 var navHeadCenter = document.getElementById("navHeadCenter");
 var navHeadSide = document.getElementsByClassName("navHeadSide");
+var navMenu = document.getElementById("navMenuContainer");
 
 var fixed = header.offsetTop;
 
-function nav() {
+function navCall() {
   if (window.pageYOffset > fixed) {
     for (var i = 0; i < navHeadSide.length; i++) {
       navHeadSide[i].classList.add("navHeadSideAni");
@@ -14,6 +15,7 @@ function nav() {
     }
     navHeadCenter.classList.add("navHeadCenterAni");
     navHeadCenter.classList.remove("navHeadCenterAni2");
+    navMenuCall();
   }
   else {
     for (var i = 0; i < navHeadSide.length; i++) {
@@ -22,5 +24,20 @@ function nav() {
     }
     navHeadCenter.classList.remove("navHeadCenterAni");
     navHeadCenter.classList.add("navHeadCenterAni2");
+    navMenuAnimation();
+  }
+}
+
+function navMenuCall() {
+  setTimeout(navMenuAnimation, 800);
+}
+
+function navMenuAnimation() {
+  if (navHeadCenter.classList.contains("navHeadCenterAni")) {
+    navMenu.classList.add("navMenuAni");
+    navMenu.classList.remove("navMenuAni2");
+  } else if (navHeadCenter.classList.contains("navHeadCenterAni2")) {
+    navMenu.classList.add("navMenuAni2");
+    navMenu.classList.remove("navMenuAni");
   }
 }
